ec4e973c91db951830cc56a44e83679ad62ab9b9
suzuka1621 <akaneakua0625@gmail.com>
2023-02-21 23:01:47 +0900

    commentモデルの作成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -15,7 +19,12 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
-  
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
   def edit
   end
 
@@ -26,12 +35,26 @@ class PrototypesController < ApplicationController
       render :edit
     end
   end
-end
 
+  def destroy
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
-
-private
+  private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
+end
