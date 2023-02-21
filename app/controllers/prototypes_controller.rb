9fd19ab30046e66401e391f50864cd1f7a7fa761
suzuka1621 <akaneakua0625@gmail.com>
2023-02-21 20:49:58 +0900

    prototypesコントローラーのnew,createアクションの追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,18 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
