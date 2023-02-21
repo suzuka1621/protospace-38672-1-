75c1a327f47de62851c25834a6fdd0eeda02fa1e
suzuka1621 <akaneakua0625@gmail.com>
2023-02-21 21:00:30 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,3 +16,9 @@ class PrototypesController < ApplicationController
     end
   end
 end
+
+private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
